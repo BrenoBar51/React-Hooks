ea3d34f6ca2729991d54bbe23189235686b8c0bc
BrenoBar51 <brenobarcelos9@gmail.com>
2023-07-31 09:46:05 -0300

    adicionando use state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react"
+import Button from "./components/Button"
+
 function App() {
+  const [buttonColor, setButtonColor] = useState('pink')
+  const [buttonSize, setButtonSize] = useState('sm')
   return (
     <>
+      <Button handleClick={() => {setButtonColor('pink')}} size='sm' color='pink' value='Botão rosa' />
+      <Button handleClick={() => {setButtonColor('orange')}} size='md' color='orange' value='Botão Laranja' />
+      <Button handleClick={() => {setButtonSize('sm')}} size='sm' color='pink' value='Botão pequeno' />
+      <Button handleClick={() => {setButtonSize('md')}} size='md' color='orange' value='Botão médio' />
+      <Button size={buttonSize} color={buttonColor} value="Botão personalizado" />
     </>
   )
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,15 @@
 export default function Button(props){
     return(
-        <button onClick={props.handleClick}>
+        <button
+            className={`
+                rounded-lg
+                ${props.color === 'pink' && 'bg-[#C92071] text-[#F5F5F5] text-center text-base font-bold'}
+                ${props.color === 'orange' && 'bg-[#ffb31f] text-[#F5F5F5] text-center text-base font-bold'}
+                ${props.size === 'sm' && 'w-28 h-10 !text-sm'}
+                ${props.size === 'md' && 'w-56 h-12'}
+            `}
+            onClick={props.handleClick}
+        >
             {props.value}
         </button>
     )
